42581b138bda39697ba56efe66e366af859563c6
Akash Madavi <akashmadavi2001@gmail.com>
2025-02-19 13:42:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onscroll = () => {
+    document.querySelectorAll('.items').forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 500;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if (top >= offset && top < offset + height) {
+            document.querySelectorAll('li a').forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    });
+}
